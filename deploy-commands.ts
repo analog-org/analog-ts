145443ded8665967b0a5552f49793c6d63178050
BossDaily <gboys2008@gmail.com>
2022-07-16 11:09:15 -0500

    changed comments

--- deploy-commands.ts
+++ deploy-commands.ts
@@ -1,25 +1,31 @@
-//Discordjs dependancy 
-const fs = require('fs')
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
-const { guildId, clientId} = require('./config.json');
+// Discordjs dependancy
 
-//dotenv dependancy
-const dotenv = require('dotenv');
+// const fs = require("fs");
+import fs from 'fs'
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v9");
+const { guildId, clientId } = require("./config.json");
+
+// dotenv dependancy
+
+const dotenv = require("dotenv");
 dotenv.config();
 //Gets slash commands
-const commands = []
-const cmdFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
+const commands = [];
+const cmdFiles = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith(".js"));
 
-const rest = new REST({ version: '9'}).setToken(process.env.TOKEN);
+const rest = new REST({ version: "9" }).setToken(process.env.TOKEN);
 
-for (const file of cmdFiles){
-	const command = require(`./commands/${file}`)
-	commands.push(command.data.toJSON())
+for (const file of cmdFiles) {
+  const command = require(`./commands/${file}`);
+  commands.push(command.data.toJSON());
 }
 
-//This actually registers the slash commands into the guild
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-	.then(() => console.log('Successfully registered application commands.'))
-	.catch(console.error);
+// This actually registers the slash commands into the guild
+rest
+  .put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+  .then(() => console.log("Successfully registered application commands."))
+  .catch(console.error);
