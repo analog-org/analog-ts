bc4027cecb9f550b84232bcb232564d089b141ee
BossDaily <gboys2008@gmail.com>
2022-07-19 17:58:31 -0500

    checks if it is a command or context menu

--- src/events/interactioncreate.ts
+++ src/events/interactioncreate.ts
@@ -1,8 +1,15 @@
+
+import { CommandInteraction, Interaction, InteractionType } from "discord.js";
+
 //This logs all interactions
 module.exports = {
-	name: 'interactionCreate',
-	execute(interaction) {
-		console.log(`At ${interaction.createdAt} ${interaction.user.tag} in #${interaction.channel.name} triggered ${interaction.type}.`);
-	},
+  name: "interactionCreate",
+  execute(interaction: Interaction) {
+    if (interaction.isCommand() || interaction.isContextMenu()) {
+      console.log(
+      `At ${interaction.createdAt} ${interaction.user.tag} in #${interaction.channel.name} triggered ${interaction.type}.`
+    );
+    }
+    
+  },
 };
- 
