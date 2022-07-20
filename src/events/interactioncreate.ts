e112754784f37ad83792b41b1205600b53fe867a
BossDaily <gboys2008@gmail.com>
2022-07-20 10:16:54 -0500

    checks if interaction happened in guild text channel

--- src/events/interactioncreate.ts
+++ src/events/interactioncreate.ts
@@ -1,4 +1,3 @@
-
 import { CommandInteraction, Interaction, InteractionType } from "discord.js";
 
 //This logs all interactions
@@ -7,10 +6,9 @@ module.exports = {
   execute(interaction: Interaction) {
     if (interaction.isCommand() || interaction.isContextMenu()) {
       if (interaction.channel?.type === "GUILD_TEXT")
-      console.log(
-      `At ${interaction.createdAt} ${interaction.user.tag} in #${interaction.channel.name} triggered ${interaction.type}.`
-    );
+        console.log(
+          `At ${interaction.createdAt} ${interaction.user.tag} in #${interaction.channel.name} triggered ${interaction.type}.`
+        );
     }
-    
   },
 };
