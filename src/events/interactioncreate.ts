070a35848fd0ac7b2ac1b5e54b6a0370f1375032
BossDaily <gboys2008@gmail.com>
2022-07-20 10:55:05 -0500

    cases for threads

--- src/events/interactioncreate.ts
+++ src/events/interactioncreate.ts
@@ -7,9 +7,14 @@ module.exports = {
     if (interaction.isCommand() || interaction.isContextMenu()) {
       switch (interaction.channel?.type) {
         case "GUILD_TEXT":
+        case "GUILD_NEWS":
           console.log(
             `${interaction.createdAt} ${interaction.user.tag} in #${interaction.channel.name} triggered a ${interaction.type} called ${interaction.commandName}.`
           );
+        case "GUILD_NEWS_THREAD":
+        case "GUILD_PUBLIC_THREAD":
+        case "GUILD_PRIVATE_THREAD":
+          
       }
     }
   },
