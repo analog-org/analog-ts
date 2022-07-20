6059bf562a66f61bc60b8c490c92be44602c66f6
BossDaily <gboys2008@gmail.com>
2022-07-20 11:04:33 -0500

    renamed and added case for DM commands

---
+++ src/events/commandLogger.ts
@@ -0,0 +1,27 @@
+import { CommandInteraction, Interaction, InteractionType } from "discord.js";
+
+//This logs all interactions
+module.exports = {
+  name: "interactionCreate",
+  execute(interaction: Interaction) {
+    if (interaction.isCommand() || interaction.isContextMenu()) {
+      switch (interaction.channel?.type) {
+        case "GUILD_TEXT":
+        case "GUILD_NEWS":
+          console.log(
+            `${interaction.createdAt}: ${interaction.user.tag} in #${interaction.channel.name} triggered a ${interaction.type} called ${interaction.commandName}.`
+          );
+        case "GUILD_NEWS_THREAD":
+        case "GUILD_PUBLIC_THREAD":
+        case "GUILD_PRIVATE_THREAD":
+          console.log(
+            `${interaction.createdAt}: ${interaction.user.tag} in #${interaction.channel.parent?.name} in thread ${interaction.channel.name} triggered a ${interaction.type} called ${interaction.commandName}.`
+          );
+        case "DM":
+          console.log(
+            `${interaction.createdAt}: ${interaction.user.tag} in DMs triggered a ${interaction.type} called ${interaction.commandName}.`
+          );
+      }
+    }
+  },
+};
