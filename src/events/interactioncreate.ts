32d4aea51797aa5b3b0f0a0bd2fefcc12b517a21
BossDaily <gboys2008@gmail.com>
2022-07-20 10:20:15 -0500

    first case for guild text

--- src/events/interactioncreate.ts
+++ src/events/interactioncreate.ts
@@ -6,7 +6,10 @@ module.exports = {
   execute(interaction: Interaction) {
     if (interaction.isCommand() || interaction.isContextMenu()) {
       switch (interaction.channel?.type) {
-        case 'GUILD_TEXT'
+        case "GUILD_TEXT":
+          console.log(
+            `${interaction.createdAt} ${interaction.user.tag} in #${interaction.channel.name} triggered a ${interaction.type} called ${interaction.commandName}.`
+          );
       }
     }
   },
