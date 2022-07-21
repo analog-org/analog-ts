fbb788793966940a8d904fd3c643ab40b0d0f6a8
BossDaily <gboys2008@gmail.com>
2022-07-21 12:53:15 -0500

    formatted code

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,33 +1,47 @@
-import { Interaction, MessageEmbed, CommandInteractionOptionResolver, Message, CommandInteraction, ApplicationCommand } from "discord.js";
+import {
+  Interaction,
+  MessageEmbed,
+  CommandInteractionOptionResolver,
+  Message,
+  CommandInteraction,
+  ApplicationCommand,
+} from "discord.js";
 
-import {SlashCommandBuilder, SlashCommandStringOption} from '@discordjs/builders';
+import {
+  SlashCommandBuilder,
+  SlashCommandStringOption,
+} from "@discordjs/builders";
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('help')
-        .setDescription('shows a list of commands for the bot'),
-    
-    async execute(interaction: CommandInteraction) {
+  data: new SlashCommandBuilder()
+    .setName("help")
+    .setDescription("shows a list of commands for the bot"),
 
-        
+  async execute(interaction: CommandInteraction) {
     try {
-        let commandsList: string | undefined
-        const client = interaction.client
-        const cmd = await client.application?.commands.fetch()
-            
-        commandsList = cmd?.map((cmd: ApplicationCommand) => `**/${cmd.name}** - ${cmd.description}`).join('\n')
+      let commandsList: string | undefined;
+      const client = interaction.client;
+      const cmd = await client.application?.commands.fetch();
 
-        const embed = new MessageEmbed()
-                  .setColor(`#6bde36`)
-                  .setTitle(`${client.user?.username}'s commands`)
-                  .setDescription(`${commandsList}`)
-                  .setThumbnail(client.user?.avatarURL({dynamic:true})!)
-        
-        await interaction.reply({embeds: [embed]}) 
+      commandsList = cmd
+        ?.map(
+          (cmd: ApplicationCommand) => `**/${cmd.name}** - ${cmd.description}`
+        )
+        .join("\n");
 
-        } catch(error) {
-            await interaction.reply({ content: 'This server has 0 commands', ephemeral: true })
-            console.error(error)
-        }
+      const embed = new MessageEmbed()
+        .setColor(`#6bde36`)
+        .setTitle(`${client.user?.username}'s commands`)
+        .setDescription(`${commandsList}`)
+        .setThumbnail(client.user?.avatarURL({ dynamic: true })!);
+
+      await interaction.reply({ embeds: [embed] });
+    } catch (error) {
+      await interaction.reply({
+        content: "This server has 0 commands",
+        ephemeral: true,
+      });
+      console.error(error);
     }
-}
+  },
+};
