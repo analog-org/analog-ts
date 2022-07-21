4600d65ca8916715f6b95fcae91ed28caf97a34d
BossDaily <gboys2008@gmail.com>
2022-07-21 11:59:32 -0500

    typings and import change

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -5,10 +5,10 @@ import fs from "fs";
 import { SlashCommandBuilder } from "@discordjs/builders";
 import { REST } from "@discordjs/rest";
 import { Routes } from "discord-api-types/v9";
-import client from "../index";
+import { client } from "../index";
 import dotenv from "dotenv";
 
-const clientId = client.user.id;
+
 
 // dotenv dependancy
 
@@ -16,18 +16,18 @@ dotenv.config();
 //Gets slash commands
 const commands: string[] = [];
 const cmdFiles = fs
-  .readdirSync(`./dist/src/commands`)
+  .readdirSync(`./src/commands`)
   .filter((file) => file.endsWith(".js"));
 
 const rest = new REST({ version: "9" }).setToken(process.env.TOKEN!);
 
 for (const file of cmdFiles) {
-  const command = require(`./dist/src/commands/${file}`);
+  const command = require(`./src/commands/${file}`);
   commands.push(command.data.toJSON());
 }
 
 // This actually registers the slash commands into the guild
-const regCMD = () => {
+const regCMD = (clientId: string) => {
   rest
     .put(Routes.applicationCommands(clientId), { body: commands })
     .then(() => console.log("Successfully registered application commands."))
