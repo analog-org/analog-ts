3da102d142f3815f6822029d9f5e0859551baa56
BossDaily <gboys2008@gmail.com>
2022-07-22 11:00:35 -0500

    changed directories

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -8,28 +8,26 @@ import { Routes } from "discord-api-types/v9";
 import { client } from "../index";
 import dotenv from "dotenv";
 
-
-
-// dotenv dependancy
-
 dotenv.config();
-//Gets slash commands
-const commands: string[] = [];
-const cmdFiles = fs
-  .readdirSync(`./src/commands`)
-  .filter((file) => file.endsWith(".js"));
-
-const rest = new REST({ version: "9" }).setToken(process.env.TOKEN!);
-
-for (const file of cmdFiles) {
-  const command = require(`./src/commands/${file}`);
-  commands.push(command.data.toJSON());
-}
-
-// This actually registers the slash commands into the guild
 const regCMD = (clientId: string) => {
+  // dotenv dependancy
+
+  //Gets slash commands
+  const commands: any[] = [];
+  const cmdFiles = fs
+    .readdirSync(`./commands`)
+    .filter((file) => file.endsWith(".js"));
+
+  const rest = new REST({ version: "9" }).setToken(process.env.TOKEN!);
+
+  for (const file of cmdFiles) {
+    const command = require(`./commands/${file}`);
+    commands.push(command.data.toJSON());
+  }
+
   rest
     .put(Routes.applicationCommands(clientId), { body: commands })
+    .then(() => console.log(commands))
     .then(() => console.log("Successfully registered application commands."))
     .catch(console.error);
 };
