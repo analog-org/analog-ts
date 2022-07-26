5d731a154d966044a03224d6ae151a3f64fa73f7
BossDaily <gboys2008@gmail.com>
2022-07-26 15:57:43 -0500

    updated types of different channels

--- src/events/commandLogger.ts
+++ src/events/commandLogger.ts
@@ -1,4 +1,4 @@
-import { CommandInteraction, Interaction, InteractionType } from "discord.js";
+import { CommandInteraction, Interaction, InteractionType, ChannelType } from "discord.js";
 
 //This logs all interactions
 module.exports = {
@@ -6,20 +6,20 @@ module.exports = {
   execute(interaction: Interaction) {
     if (interaction.type === InteractionType.ApplicationCommand) {
       switch (interaction.channel?.type) {
-        case "GUILD_TEXT":
-        case "GUILD_NEWS":
+        case (ChannelType.GuildText):
+        case (ChannelType.GuildNews):
           console.log(
             `${interaction.createdAt}: ${interaction.user.tag} in #${interaction.channel.name} triggered a ${interaction.type} called ${interaction.commandName}.`
           );
           break;
-        case "GUILD_NEWS_THREAD":
-        case "GUILD_PUBLIC_THREAD":
-        case "GUILD_PRIVATE_THREAD":
+        case (ChannelType.GuildNewsThread):
+        case (ChannelType.GuildPublicThread):
+        case (ChannelType.GuildPrivateThread):
           console.log(
             `${interaction.createdAt}: ${interaction.user.tag} in #${interaction.channel.parent?.name} in thread ${interaction.channel.name} triggered a ${interaction.type} called ${interaction.commandName}.`
           );
           break;
-        case "DM":
+        case (ChannelType.DM):
           console.log(
             `${interaction.createdAt}: ${interaction.user.tag} in DMs triggered a ${interaction.type} called ${interaction.commandName}.`
           );
