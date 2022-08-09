fdd0fdd7af0eae9a1f4426cfc9ba674f20a7af16
BossDaily <gboys2008@gmail.com>
2022-08-09 14:39:24 -0500

    added basic embed

--- src/components/buttons/botInfo.ts
+++ src/components/buttons/botInfo.ts
@@ -1,5 +1,5 @@
 import { ButtonInteraction, EmbedBuilder } from "discord.js";
-import { client } from '../../../index'
+import { client } from "../../../index";
 
 module.exports = {
   data: {
@@ -7,14 +7,13 @@ module.exports = {
   },
   async execute(interaction: ButtonInteraction) {
     try {
-
       const embed = new EmbedBuilder()
         .setColor(`#6bde36`)
         .setTitle(`${client.user?.username}'s commands`)
         .setDescription(`${client.user.username}`)
         .setThumbnail(client.user?.avatarURL({ forceStatic: false })!);
 
-      await interaction.reply({ embeds: [embed]});
+      await interaction.reply({ embeds: [embed] });
     } catch (error) {
       await interaction.reply({
         content: "This server has 0 commands",
