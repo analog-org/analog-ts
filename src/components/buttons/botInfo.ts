ab2b93bd02fb41c3d833cfbf8f27ce64cfa348dd
BossDaily <gboys2008@gmail.com>
2022-08-09 14:38:01 -0500

    bot info

--- src/components/buttons/botInfo.ts
+++ src/components/buttons/botInfo.ts
@@ -1,10 +1,26 @@
-import { ButtonInteraction } from "discord.js";
+import { ButtonInteraction, EmbedBuilder } from "discord.js";
+import { client } from '../../../index'
 
 module.exports = {
   data: {
-    name: `botInfo`
+    name: `botInfo`,
   },
   async execute(interaction: ButtonInteraction) {
+    try {
 
-  }
-}
+      const embed = new EmbedBuilder()
+        .setColor(`#6bde36`)
+        .setTitle(`${client.user?.username}'s commands`)
+        .setDescription(`${client.user.username}`)
+        .setThumbnail(client.user?.avatarURL({ forceStatic: false })!);
+
+      await interaction.reply({ embeds: [embed]});
+    } catch (error) {
+      await interaction.reply({
+        content: "This server has 0 commands",
+        ephemeral: true,
+      });
+      console.error(error);
+    }
+  },
+};
