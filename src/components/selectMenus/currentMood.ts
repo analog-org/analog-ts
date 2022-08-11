6b96529df2cb0aa1060387e6fc32a045e9b580fa
BossDaily <gboys2008@gmail.com>
2022-08-11 17:57:09 -0500

    removed embed and added proper emotion

--- src/components/selectMenus/currentMood.ts
+++ src/components/selectMenus/currentMood.ts
@@ -1,5 +1,15 @@
-
-import { ActionRow, ActionRowBuilder, ButtonInteraction, EmbedBuilder, ModalActionRowComponentBuilder, ModalBuilder, ModalSubmitInteraction, SelectMenuInteraction, TextInputBuilder, TextInputStyle } from "discord.js";
+import {
+  ActionRow,
+  ActionRowBuilder,
+  ButtonInteraction,
+  EmbedBuilder,
+  ModalActionRowComponentBuilder,
+  ModalBuilder,
+  ModalSubmitInteraction,
+  SelectMenuInteraction,
+  TextInputBuilder,
+  TextInputStyle,
+} from "discord.js";
 import { client } from "../../../index";
 
 module.exports = {
@@ -7,28 +17,24 @@ module.exports = {
     name: `currentMood`,
   },
   async execute(interaction: SelectMenuInteraction) {
+    const emotion = interaction.values[0]
     try {
-      const embed = new EmbedBuilder()
-        .setColor(`#6bde36`)
-        .setTitle(`${client.user?.username}'s commands`)
-        .setDescription(`${client.user.username}`)
-        .setThumbnail(client.user?.avatarURL({ forceStatic: false })!);
+      
 
       const modal = new ModalBuilder()
-        .setTitle('Why do you feel that way?')
-        .setCustomId('explainMood')
+        .setTitle(`Why are you ${emotion}?`)
+        .setCustomId("explainMood")
         .setComponents(
-          new ActionRowBuilder<ModalActionRowComponentBuilder>()
-            .addComponents( 
-              new TextInputBuilder()
-                .setLabel('Why do you feel that way?')
-                .setStyle(TextInputStyle.Paragraph)
-                .setPlaceholder('Explain why you feel this way')
-                .setCustomId('explain_mood')
-            )
-        )
+          new ActionRowBuilder<ModalActionRowComponentBuilder>().addComponents(
+            new TextInputBuilder()
+              .setLabel(`Why are you ${emotion}?`)
+              .setStyle(TextInputStyle.Paragraph)
+              .setPlaceholder(`Explain why your ${emotion}`)
+              .setCustomId("explain_mood")
+          )
+        );
       
-      await interaction.showModal(modal)
+      await interaction.showModal(modal);
     } catch (error) {
       await interaction.reply({
         content: "This server has 0 commands",
