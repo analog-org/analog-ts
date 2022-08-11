3fba1b0cde1eb3df945941a90d72b5ef78720314
BossDaily <gboys2008@gmail.com>
2022-08-11 17:16:15 -0500

    added selectmenu builder

--- src/components/buttons/botInfo.ts
+++ src/components/buttons/botInfo.ts
@@ -1,4 +1,12 @@
-import { ButtonInteraction, EmbedBuilder } from "discord.js";
+import {
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonInteraction,
+  ButtonStyle,
+  EmbedBuilder,
+  MessageActionRowComponentBuilder,
+  SelectMenuBuilder,
+} from "discord.js";
 import { client } from "../../../index";
 
 module.exports = {
@@ -7,13 +15,19 @@ module.exports = {
   },
   async execute(interaction: ButtonInteraction) {
     try {
+      const row =
+        new ActionRowBuilder<MessageActionRowComponentBuilder>().addComponents(
+          new SelectMenuBuilder()
+            .setCustomId("currentMood")
+            .setOptions([{ label: "happy", value: "option_one", emoji: "😃" }, { label: 'sad', value: 'option_two', emoji: "😭"}, { label: 'mischievous', value: 'option_three', emoji: "😈"}])
+        );
       const embed = new EmbedBuilder()
         .setColor(`#6bde36`)
         .setTitle(`${client.user?.username}'s commands`)
         .setDescription(`${client.user.username}`)
-        .setThumbnail(client.user?.avatarURL({ forceStatic: false })!);
+        .setThumbnail(client.user?.avatarURL({ forceStatic: false }));
 
-      await interaction.reply({ embeds: [embed] });
+      await interaction.reply({ embeds: [embed], components: [row] });
     } catch (error) {
       await interaction.reply({
         content: "This server has 0 commands",
