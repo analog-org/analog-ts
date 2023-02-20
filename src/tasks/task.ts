b9e132c417ddc8c4f60f3b0adb2469c52587504e
BossDaily <gboys2008@gmail.com>
2023-02-20 10:51:03 -0600

    commented out boilerplate code

---
+++ src/tasks/task.ts
@@ -0,0 +1,9 @@
+/* 
+import cron from 'node-cron';
+
+export default function(): void {
+  // Schedule a job to run every day at 3 PM
+ // cron.schedule('* * * * *', () => {
+    console.log('Task 2 is running');
+  });
+};  */
